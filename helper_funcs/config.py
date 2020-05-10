b4535acd8e42bca6d2a3cc369808932d7db7ef59
reyan421 <65124122+reyan421@users.noreply.github.com>
2020-05-10 22:27:59 +0530

    Add files via upload

---
+++ helper_funcs/config.py
@@ -0,0 +1,11 @@
+ from sample_config import Config
+
+
+class Development(Config):
+  APP_ID = 1259167
+  API_HASH = "c2cfc1b3e78a1646117f0cccd4e2c22a"
+  TG_BOT_TOKEN = "1129227461:AAEOVG041LWdPlTtHB1pYVDQ9cMAh8GIcBw"
+  AUTH_USERS = [
+      "189583261"
+      # is a string for LEGACY purposes
+  ]
